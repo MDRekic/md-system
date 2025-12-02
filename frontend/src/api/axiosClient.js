cce7fb08fc323b99b9fc70d6a2628c0c9aeaf785
mydienst <ermin.rekic@mydienst.de>
2025-12-02 23:17:24 +0100

    Initial commit md-system

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,11 +1,19 @@
+// frontend/src/api/axiosClient.js
 import axios from "axios";
 
-// u produkciji će sve ići preko Nginx-a na /api
 const API_URL = import.meta.env.VITE_API_URL || "/api";
 
 const axiosClient = axios.create({
   baseURL: API_URL,
-  withCredentials: true,
+  withCredentials: false,
+});
+
+axiosClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem("comp4_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default axiosClient;
